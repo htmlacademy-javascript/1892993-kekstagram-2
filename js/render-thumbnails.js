28c1bb400193d773a2f2eb5e0a0028ebda601202
Sergey <serbog230797@gmail.com>
2025-10-09 20:51:49 +0300

    оптимизация

--- js/render-thumbnails.js
+++ js/render-thumbnails.js
@@ -6,16 +6,16 @@ const elementPicture = document.querySelector('#picture')
 const fragment = document.createDocumentFragment();
 let thumbnails = [];
 
-const renderThumbnail = (element) => {
+const renderThumbnail = ({url, description, likes, comments}) => {
   const picture = elementPicture.cloneNode(true);
   const pictureImg = picture.querySelector('.picture__img');
   const pictureLikes = picture.querySelector('.picture__likes');
   const pictureComments = picture.querySelector('.picture__comments');
 
-  pictureImg.src = element.url;
-  pictureImg.alt = element.description;
-  pictureLikes.textContent = element.likes;
-  pictureComments.textContent = element.comments.length;
+  pictureImg.src = url;
+  pictureImg.alt = description;
+  pictureLikes.textContent = likes;
+  pictureComments.textContent = comments.length;
   fragment.appendChild(picture);
   sectionPictures.appendChild(fragment);
 };
